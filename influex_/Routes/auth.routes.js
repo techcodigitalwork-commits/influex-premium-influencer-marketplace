019a191146db9c9e9ea9c9708ecfd4b0662718e8
techcodigitalwork-commits <techco.digitalwork@gmail.com>
2026-02-10 12:40:38 +0000

    Routes

--- influex_/Routes/auth.routes.js
+++ influex_/Routes/auth.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/register", register);
+router.post("/signup", signup);
 
 export default router;
 
