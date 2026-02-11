22bcab528eb83d8d7bdce9abaeda8b7f94e67090
techcodigitalwork-commits <techco.digitalwork@gmail.com>
2026-02-11 10:13:33 +0000

    profile routes

--- influex_/Routes/profile.routes.js
+++ influex_/Routes/profile.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { createProfile, getMyProfile } from "../controllers/profile.controller.js";
-import { protect } from "../middlewares/auth.middleware.js";
+import auth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, createProfile);
-router.get("/me", protect, getMyProfile);
+router.post("/", auth, createProfile);
+router.get("/me", auth, getMyProfile);
 
 export default router;
