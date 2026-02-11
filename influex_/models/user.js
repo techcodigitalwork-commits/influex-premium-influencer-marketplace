af2556a1e2c9e517bce9a6b6a3b899ae67551e89
techcodigitalwork-commits <techco.digitalwork@gmail.com>
2026-02-11 11:28:52 +0000

    update user.js

--- influex_/models/user.js
+++ influex_/models/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
 
   role: {
     type: String,
-    enum: ["Brand", "Influencer", "Model", "Photographer"],
+    enum: ["brand", "influencer", "model", "photographer"],
     required: true
   },
 
