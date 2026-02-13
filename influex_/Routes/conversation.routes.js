75eaf490117eedbcc8e2a034241ca7cfbb6f25f2
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-13 02:27:15 -0800

    message

--- influex_/Routes/conversation.routes.js
+++ influex_/Routes/conversation.routes.js
@@ -1,16 +1,16 @@
 import express from "express";
-import { verifyToken } from "../middlewares/auth.middleware.js";
+import auth from "../middlewares/auth.middleware.js";
 import { getOrCreateConversation, sendMessage, getMessages } from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
 // Create or fetch conversation
-router.post("/create", verifyToken, getOrCreateConversation);
+router.post("/create", auth, getOrCreateConversation);
 
 // Send message
-router.post("/send/:conversationId", verifyToken, sendMessage);
+router.post("/send/:conversationId", auth, sendMessage);
 
 // Get all messages in conversation
-router.get("/messages/:conversationId", verifyToken, getMessages);
+router.get("/messages/:conversationId", auth, getMessages);
 
 export default router;
