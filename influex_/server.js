6212275f6d965cc5b416769435a580fce4d70c78
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-23 02:36:18 -0800

    message

--- influex_/server.js
+++ influex_/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+// Load env
+dotenv.config();
 import mongoose from "mongoose";
 import http from "http";
 import { Server } from "socket.io";
