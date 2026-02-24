022ca0f572e1edbddcf1bccb9adcfa871e0c59a1
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-24 04:29:33 -0800

    message

--- influex_/Routes/conversation.routes.js
+++ influex_/Routes/conversation.routes.js
@@ -1,8 +1,23 @@
 import express from "express";
 import auth from "../middlewares/auth.middleware.js";
 import { getOrCreateConversation, sendMessage, getMessages } from "../controllers/conversation.controller.js";
-
+import Conversation from "../models/Conversation.js";
 const router = express.Router();
+router.get("/my", auth, async (req, res) => {
+  try {
+    const conversations = await Conversation.find({
+      participants: req.user.id
+    })
+      .populate("participants", "name email")
+      .sort({ updatedAt: -1 });
+
+    res.json({ success: true, data: conversations });
+  } catch (err) {
+    console.error("MY CONVERSATIONS ERROR:", err);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
 
 // Create or fetch conversation
 router.post("/create", auth, getOrCreateConversation);
