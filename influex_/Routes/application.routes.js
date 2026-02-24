16650e35535e2c9bf5f56e6de9dac7a8378fe8e3
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-24 02:43:46 -0800

    message

--- influex_/Routes/application.routes.js
+++ influex_/Routes/application.routes.js
@@ -1,7 +1,7 @@
 // src/routes/application.routes.js
 import express from "express";
 import { decideApplication ,getMyApplications} from "../controllers/application.controller.js";
-import auth from "../middlewares/auth.js";
+import auth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
