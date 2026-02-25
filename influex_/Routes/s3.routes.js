ea6917d1e06ed31e4f20837e71439342839e10a4
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-25 02:41:10 -0800

    message

--- influex_/Routes/s3.routes.js
+++ influex_/Routes/s3.routes.js
@@ -1,8 +1,26 @@
 import express from "express";
-import { testUpload } from "../controllers/s3controller.js";
+import auth from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
-router.get("/test-s3", testUpload);
+router.post(
+  "/upload/image",
+  auth,
+  upload.single("image"),
+  (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "No file uploaded"
+      });
+    }
+
+    res.json({
+      success: true,
+      url: req.file.location   // 🔥 S3 public URL
+    });
+  }
+);
 
 export default router;
