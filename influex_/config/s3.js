84c2721ab5277794092c0e2f9358fbc896e83f08
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-25 04:18:19 -0800

    message

--- influex_/config/s3.js
+++ influex_/config/s3.js
@@ -10,10 +10,10 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION?.trim(),
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID?.trim(),
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY?.trim(),
   },
 }); //console.log("AWS keys:", process.env.AWS_ACCESS_KEY,
 //  process.env.AWS_SECRET_KEY, process.env.AWS_REGION);
