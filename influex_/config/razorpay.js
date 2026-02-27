db4b75c8bd56cc54e951c5423f4a67e6648d5cb8
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-27 02:45:10 -0800

    message

--- influex_/config/razorpay.js
+++ influex_/config/razorpay.js
@@ -1,10 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import Razorpay from "razorpay";
-console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
-console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-export default razorpay;
+export { razorpay };
