81bc0957ed38e3be5eb33a8c7f9552884d993eca
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-27 02:57:16 -0800

    message

--- influex_/config/razorpay.js
+++ influex_/config/razorpay.js
@@ -1,12 +1,21 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import Razorpay from "razorpay";
+
+// Resolve absolute path of .env
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
+console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
 
 let razorpayInstance;
 
 export const getRazorpayInstance = () => {
   if (!razorpayInstance) {
-    console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
-    console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
-
     razorpayInstance = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_KEY_SECRET,
