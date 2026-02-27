dab16ac37349bc7b7186843011a4b2c88a7dc4e0
Ranu Patel <techco.digitalwork@gmail.com>
2026-02-27 02:00:10 -0800

    message

--- influex_/config/razorpay.js
+++ influex_/config/razorpay.js
@@ -4,3 +4,11 @@ export const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
+import Razorpay from "razorpay";
+
+export const getRazorpayInstance = () => {
+  return new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+};
