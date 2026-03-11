52ca028e311b152a6d817be1723e362cdeef5139
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-11 01:39:34 -0700

    message

---
+++ influex_/models/invite.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const inviteSchema = new mongoose.Schema({
+
+  campaignId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Campaign",
+    required: true
+  },
+
+  creatorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  brandId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  status: {
+    type: String,
+    enum: ["pending","accepted","rejected"],
+    default: "pending"
+  }
+
+},{timestamps:true})
+
+export default mongoose.model("Invite",inviteSchema)
