2a17774879c145de296eb5d5b0d17d6d261fea77
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-11 04:02:36 -0700

    message

--- influex_/Routes/deal.routes.js
+++ influex_/Routes/deal.routes.js
@@ -1,10 +1,14 @@
 import express from "express"
-import {createDeal,depositPayment,approveWork} from "../controllers/deal.controller.js"
+import {createDeal,depositPayment,approveWork,getMyDeals,getDealById,getCampaignDeals} from "../controllers/deal.controller.js"
+import auth from "../middlewares/auth.middleware.js"
 
 const router = express.Router()
 
-router.post("/create",createDeal)
-router.post("/deposit",depositPayment)
-router.post("/approve",approveWork)
+router.post("/create",auth,createDeal)
+router.post("/deposit",auth,depositPayment)
+router.post("/approve",auth,approveWork)
+router.get("/my",auth,getMyDeals)
+router.get("/:id",auth,getDealById)
+router.get("/campaign/:campaignId",auth,getCampaignDeals)
 
 export default router
--- influex_/controllers/deal.controller.js
+++ influex_/controllers/deal.controller.js
@@ -3,7 +3,7 @@ import Deal from "../models/deal.js"
 export const createDeal = async (req,res)=>{
  try{
 
- const {campaignId, creatorId, amount} = req.body
+ const {campaignId, influencerId , amount} = req.body
 
  const commission = amount * 0.10
  const creatorAmount = amount - commission
@@ -47,4 +47,67 @@ export const approveWork = async (req,res)=>{
  await deal.save()
 
  res.json({message:"Payment released"})
+}
+export const getMyDeals = async (req,res)=>{
+ try{
+
+ let deals
+
+ if(req.user.role === "brand"){
+   deals = await Deal.find({
+     brandId:req.user._id
+   }).populate("influencerId","username email")
+ }
+
+ if(req.user.role === "influencer"){
+   deals = await Deal.find({
+     influencerId:req.user._id
+   }).populate("campaignId","title budget")
+ }
+
+ res.json({
+   success:true,
+   data:deals
+ })
+
+ }catch(err){
+  res.status(500).json({message:err.message})
+ }
+}
+export const getDealById = async (req,res)=>{
+ try{
+
+ const {id} = req.params
+
+ const deal = await Deal.findById(id)
+   .populate("campaignId")
+   .populate("brandId","username email")
+   .populate("influencerId","username email")
+
+ if(!deal){
+  return res.status(404).json({
+   message:"Deal not found"
+  })
+ }
+
+ res.json(deal)
+
+ }catch(err){
+  res.status(500).json({message:err.message})
+ }
+}
+export const getCampaignDeals = async (req,res)=>{
+ try{
+
+ const {campaignId} = req.params
+
+ const deals = await Deal.find({
+   campaignId
+ }).populate("influencerId","username email")
+
+ res.json(deals)
+
+ }catch(err){
+  res.status(500).json({message:err.message})
+ }
 }
