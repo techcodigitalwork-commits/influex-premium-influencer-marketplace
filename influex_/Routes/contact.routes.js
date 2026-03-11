6ff1648cc2db70402c4f564ab4881608a3831021
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-11 03:36:13 -0700

    message

--- influex_/Routes/contact.routes.js
+++ influex_/Routes/contact.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
 import {unlockContact} from "../controllers/contact.controller.js"
+import auth from "../middlewares/auth.middleware.js"
 
 const router = express.Router()
 
-router.post("/unlock",unlockContact)
+router.post("/unlock",auth,unlockContact)
 
 export default router
--- influex_/controllers/contact.controller.js
+++ influex_/controllers/contact.controller.js
@@ -2,9 +2,19 @@ import User from "../models/user.js"
 
 export const unlockContact = async (req,res)=>{
 
+ try{
+
+ if(!req.user){
+   return res.status(401).json({message:"User not authenticated"})
+ }
+
  const {influencerId} = req.body
 
  const brand = await User.findById(req.user._id)
+
+ if(!brand){
+   return res.status(404).json({message:"Brand not found"})
+ }
 
  if(brand.bits < 50){
   return res.status(400).json({message:"Not enough bits"})
@@ -15,8 +25,16 @@ export const unlockContact = async (req,res)=>{
 
  const creator = await User.findById(influencerId)
 
+ if(!creator){
+   return res.status(404).json({message:"Creator not found"})
+ }
+
  res.json({
    email:creator.email
  })
+
+ }catch(err){
+  res.status(500).json({message:err.message})
+ }
 
 }
