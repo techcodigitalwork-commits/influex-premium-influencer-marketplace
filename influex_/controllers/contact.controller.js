a6982c863738c84a9133420ef6de7a0ba5b08800
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-11 03:30:48 -0700

    message

--- influex_/controllers/contact.controller.js
+++ influex_/controllers/contact.controller.js
@@ -4,7 +4,7 @@ export const unlockContact = async (req,res)=>{
 
  const {influencerId} = req.body
 
- const brand = await User.findById(req.user.id)
+ const brand = await User.findById(req.user._id)
 
  if(brand.bits < 50){
   return res.status(400).json({message:"Not enough bits"})
