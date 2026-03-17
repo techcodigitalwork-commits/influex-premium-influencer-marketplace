6c9818c8b24e0bc84ca50d4e3f3a95fe1171be7c
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-17 13:56:59 +0530

    message

--- influex_/models/user.js
+++ influex_/models/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
 
   role: {
     type: String,
-    enum: ["brand", "influencer"],
+    enum: ["brand", "influencer", "admin"],
     required: true
   },
 
