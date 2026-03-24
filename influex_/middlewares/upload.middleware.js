c79fc08a6d39db45c46a40040bb889383d110d8f
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-24 16:49:19 +0530

    message

--- influex_/middlewares/upload.middleware.js
+++ influex_/middlewares/upload.middleware.js
@@ -2,23 +2,15 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import { s3 } from "../config/s3.js";
 
-const uploadImage = multer({
+const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.AWS_BUCKET_NAME,
+    bucket: process.env.AWS_BUCKET_NAME, // ✅ DIRECT VALUE
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
       cb(null, `images/${Date.now()}-${file.originalname}`);
     },
   }),
-
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only images allowed"), false);
-    }
-  },
 });
 
-export default uploadImage;
+export default upload;
