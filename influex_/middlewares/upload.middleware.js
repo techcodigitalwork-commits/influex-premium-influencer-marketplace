afaa3997cd1b01f479480a4734c42e5665183578
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-24 16:51:29 +0530

    message

--- influex_/middlewares/upload.middleware.js
+++ influex_/middlewares/upload.middleware.js
@@ -5,12 +5,16 @@ import { s3 } from "../config/s3.js";
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.AWS_BUCKET_NAME, // ✅ DIRECT VALUE
+    bucket: (req, file, cb) => {
+      cb(null, process.env.AWS_BUCKET_NAME);
+    },
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
-      cb(null, `images/${Date.now()}-${file.originalname}`);
+      cb(null, `profiles/${Date.now()}-${file.originalname}`);
     },
   }),
+  
 });
 
 export default upload;
+
