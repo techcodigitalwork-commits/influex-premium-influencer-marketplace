73fad4a96d1c49dfb8ee5d94631751eaf4edccf3
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-24 17:02:19 +0530

    message

--- influex_/middlewares/upload.middleware.js
+++ influex_/middlewares/upload.middleware.js
@@ -1,10 +1,12 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import { s3 } from "../config/s3.js";
+console.log("BUCKET:", process.env.AWS_BUCKET_NAME);
 
 const upload = multer({
   storage: multerS3({
     s3,
+  
     bucket: process.env.AWS_BUCKET_NAME, // ✅ FIXED
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
