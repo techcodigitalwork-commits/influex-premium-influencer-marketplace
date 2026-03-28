1c2df35a650dd788f125fbd5a6ac14902360e83b
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-28 16:27:42 +0530

    message

--- influex_/utils/sendEmail.js
+++ influex_/utils/sendEmail.js
@@ -4,13 +4,13 @@ import nodemailer from "nodemailer";
 // Configure transporter
 // --------------------------
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: Number(process.env.EMAIL_PORT) || 465,
-  secure: Number(process.env.EMAIL_PORT) === 465, // 🔥 auto handle
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // 🔥 MUST
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 // --------------------------
