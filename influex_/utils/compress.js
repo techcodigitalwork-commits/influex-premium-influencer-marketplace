8ef549b0289b959a875213b3ff6bbbd8862f7789
Ranu Patel <techco.digitalwork@gmail.com>
2026-03-28 14:43:40 +0530

    message

--- influex_/utils/compress.js
+++ influex_/utils/compress.js
@@ -6,12 +6,21 @@ export const compressVideo = (inputPath, outputPath) => {
       .outputOptions([
         "-vf scale=720:-2",
         "-vcodec libx264",
-        "-crf 28",
-        "-preset fast"
+       "-crf 28",
+  "-maxrate 1000k",
+  "-bufsize 2000k",
+        "-preset fast",
+        "-movflags +faststart"
       ])
       .save(outputPath)
       .on("end", () => {
         console.log("✅ Video compressed");
+          // 🔥 YAHI ADD KARNA HAI
+      const originalSize = fs.statSync(inputPath).size / (1024 * 1024);
+const compressedSize = fs.statSync(outputPath).size / (1024 * 1024);
+
+console.log("📦 Original:", originalSize.toFixed(2), "MB");
+console.log("📦 Compressed:", compressedSize.toFixed(2), "MB");
         resolve();
       })
       .on("error", (err) => {
